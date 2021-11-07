6942df253d1df98e3cbc667a8f657a91dd713ba0
Sweety Sutar <74287428+sweetysutar@users.noreply.github.com>
2021-11-07 17:41:39 +0530

    Add files via upload

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,36 @@
+function startGame() {
+var randomNumber1 = Math.ceil(Math.random() * 6);
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "./images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 =  Math.ceil(Math.random() * 6);;
+
+var randomDiceImage2 = "./images/dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomDiceImage2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "👑 Player 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 👑";
+
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
+}
+
+startGame();
+
+//const replayButton = document.getElementById("replay");
+
+//replayButton.addEventListener("click",startGame);
